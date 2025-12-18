9b0e0ff743c81d1ad9994af03155e0998aeccb1e
unknown <rajshivam2745@gmail.com>
2025-12-18 22:52:30 +0530

    auth.ts file authentication fixes

--- server/src/auth/auth.ts
+++ server/src/auth/auth.ts
@@ -1,4 +1,4 @@
-import jsonwebtoken from "jsonwebtoken";
+import jsonwebtoken, { SignOptions } from "jsonwebtoken";
 
 interface Token {
   jwt: string;
@@ -9,14 +9,21 @@ interface Token {
 export async function generateAuthToken(
   userId: string,
   expiryTime: string | number
-): Token {
+): Promise<Token> {
+  const signOptions: SignOptions = {
+    expiresIn: expiryTime || "2d",
+    algorithm: "HS256",
+  };
   const token = jsonwebtoken.sign(
-    userId,
+    { userId },
     process.env.JWT_SECRET_KEY as string,
-    {
-      expiresIn: expiryTime || "2d",
-      algorithm: "HS256",
-    }
+    signOptions
   );
-  return token;
+  const refreshTime = new Date();
+  const expiryDate = new Date(refreshTime.getTime() + (7 * 24 * 60 * 60 * 1000)); // 7 days example
+  return {
+    jwt: token,
+    refreshTime,
+    expiryTime: expiryDate,
+  };
 }
